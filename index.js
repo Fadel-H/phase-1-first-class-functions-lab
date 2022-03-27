8c7ccdacd56bd3cf8f40f2a116a09c65684db188
Fadel <fadel.g.heji@gmail.com>
2022-03-26 21:47:42 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(array){
+    const newArray =[...array]
+    return newArray.slice(0,2)
+}
+
+const returnLastTwoDrivers =  function(array){
+    const newArray2 =[...array]
+    return newArray2.splice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function fareAmount(fare){
+        return fare*number
+    }
+}
+
+const fareDoubler = function fareAmount(fare){
+    return fare*2
+}
+
+const fareTripler = function fareAmount(fare){
+    return fare*3
+}
+
+function selectDifferentDrivers(drivers, argument){
+    return argument(drivers)
+}
